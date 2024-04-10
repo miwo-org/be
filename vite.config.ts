26306984600094e2613cad67978ae99465f7cf25
mihewo <166595931+mihewo@users.noreply.github.com>
2024-04-10 15:13:48 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,8 +12,12 @@ export default defineConfig({
   },
   test: {
     environment: "jsdom",
-    coverage: {
+ coverage: {
       reporter: ["text", "json", "json-summary"],
+      lines: 100,
+      branches: 100,
+      functions: 100,
+      statements: 100
     },
     setupFiles: ["./test/setup.ts"],
   },
